8a3d3013f0cf73db48f531403a3a1790d76cc54d
CalebCouch <calebblackcat@gmail.com>
2025-04-05 12:24:02 -0400

    test

--- src/elements/shapes.rs
+++ src/elements/shapes.rs
@@ -1,5 +1,5 @@
 use rust_on_rails::prelude::*;
-use crate::layout::Stack;
+use crate::layout::{Offset, Padding, Size, Stack};
 
 pub struct Circle;
 
@@ -52,32 +52,35 @@ impl Events for RoundedRectangle {
     }
 }
 
-//  #[derive(Debug, Component)]
-//  pub struct Rectangle(Stack, Shape);
+#[derive(Debug, Component)]
+pub struct Rectangle(Stack, Shape);
 
-//  impl Rectangle {
-//      pub fn new(c: Color) -> Self {
-//          Rectangle(
-//              Stack(Offset::default(), Offset::default(), Self::get_size(None), Self::get_size(None), Padding::default()),
-//              Shape(ShapeType::Rectangle(0, (0, 0)), c)
-//          )
-//      }
+impl Rectangle {
+    pub fn new(c: Color) -> Self {
+        Rectangle(
+            Stack(Offset::default(), Offset::default(), Self::get_size(None), Self::get_size(None), Padding::default()),
+            Shape(ShapeType::Rectangle(0, (0, 0)), c)
+        )
+    }
 
-//      pub fn shape(&mut self) -> &mut Shape { &mut self.1 }
+    pub fn shape(&mut self) -> &mut Shape { &mut self.1 }
 
-//      fn get_size(s: Option<u32>) -> Size {
-//          s.map(|s| Size::Static(s)).unwrap_or(Size::Fill(0, u32::MAX))
-//      }
-//  }
+    fn get_size(s: Option<u32>) -> Size {
+        s.map(|s| Size::Static(s)).unwrap_or(Size::Fill(0, u32::MAX))
+    }
+}
 
-//  impl Events for Rectangle {
-//      fn on_resize(&mut self, _ctx: &mut Context, size: (u32, u32)) {
-//          if let Shape(ShapeType::Rectangle(_, (w, h)), _) = &mut self.1 {
-//              *w = size.0;
-//              *h = size.1;
-//          }
-//      }
-//  }
+impl Events for Rectangle {
+    fn on_event(&mut self, _ctx: &mut Context, event: &mut dyn Event) -> bool {
+        if let Some(ResizeEvent(size)) = event.downcast_ref() {
+            if let Shape(ShapeType::Rectangle(_, (w, h)), _) = &mut self.1 {
+                *w = size.0;
+                *h = size.1;
+            }
+        }
+        true
+    }
+}
 
 pub struct Outline;
 
